3394b316518c7b3faf4d6aedbc49115e87592f87
Thomas Jouaux <thomasjouau@gmail.com>
2022-12-14 14:18:57 +0100

    ok

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
@@ -19,7 +18,7 @@ class AdminController extends AbstractController
         
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
-            'admin' => $admin,
         ]);
     }
 }
+
